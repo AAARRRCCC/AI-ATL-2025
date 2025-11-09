10f21931860b1e4f4543d492f7c82fd8d649108d
Elijah.Borum <elijah.borum@gmail.com>
2025-11-08 19:58:14 -0500

    Update ChatInput.tsx

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -59,7 +59,6 @@ export function ChatInput({
   return (
     <div className="border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 p-4">
       <div className="flex gap-2">
-        <div className="flex-1 relative">
           <textarea
             ref={textareaRef}
             value={message}
@@ -79,7 +78,6 @@ export function ChatInput({
               {characterCount}/{maxChars}
             </span>
           )}
-        </div>
 
         <button
           onClick={handleSend}
